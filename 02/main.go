5bafe78662536efc7af8d53184a096788dd44788
Zak Stroud <ztstroud@gmail.com>
2025-12-02 19:26:10 -0800

    feat(02): computer sum in full range
    Computing the sum in a full range is straight forward enough, so I
    started there.

--- 02/main.go
+++ 02/main.go
@@ -6,3 +6,31 @@ func numDigits(val uint64) int {
 	return int(math.Log10(float64(val))) + 1
 }
 
+func pow10(val int) uint64 {
+	return uint64(math.Pow10(val))
+}
+
+func countRepeated(lower, upper uint64) uint64 {
+	lowerDigits := numDigits(lower)
+	upperDigits := numDigits(upper)
+
+	count := uint64(0)
+	for digits := lowerDigits; digits <= upperDigits; digits += 1 {
+		// Odd number of digits can never have repeats
+		if digits % 2 == 1 {
+			continue
+		}
+
+		halfMagnitude := pow10(digits / 2)
+
+		countStart := pow10(digits / 2 - 1)
+		countEnd := halfMagnitude - 1
+
+		for halfVal := countStart; halfVal <= countEnd; halfVal += 1 {
+			count += halfVal + halfVal * halfMagnitude
+		}
+	}
+
+	return count
+}
+
--- 02/main_test.go
+++ 02/main_test.go
@@ -35,3 +35,47 @@ func TestNumDigits(t *testing.T) {
 	}
 }
 
+func TestCountRepeatedFullRange(t *testing.T) {
+	cases := []struct {
+		name string
+		lower, upper uint64
+		expected uint64
+	}{
+		{
+			"SingleDigit",
+			1, 9,
+			0,
+		},
+		{
+			"DoubleDigit",
+			10, 99,
+			495,
+		},
+		{
+			"TripleDigit",
+			100, 999,
+			0,
+		},
+		{
+			"QuadrupleDigit",
+			1000, 9999,
+			495405,
+		},
+		{
+			"SingleToQuadrupleDigit",
+			1, 9999,
+			495900,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			digits := countRepeated(c.lower, c.upper)
+			
+			if digits != c.expected {
+				t.Errorf("Expected %d to be %d", digits, c.expected)
+			}
+		})
+	}
+}
+
