3b4e5c792ec5437eb57820efd28238290699dfd5
Zak Stroud <ztstroud@gmail.com>
2025-12-03 17:59:31 -0800

    feat(03): compute the largest joltage from a battery bank

--- 03/main.go
+++ 03/main.go
@@ -19,3 +19,17 @@ func firstHighestIndex(vs []int) int {
 	panic("highest value in slice is no longer present")
 }
 
+func largestJolt(bank []int) int {
+	// You always want to use the largest digit you can for the 10s place. Using
+	// a lower digit will always result in a smaller number. However, you cannot
+	// use the last number because you would have no 1s place digit.
+	firstIndex := firstHighestIndex(bank[:len(bank) - 1])
+
+	// Out of the remaining digits, we always want the biggest one to make the
+	// largest overall number
+	remainingStart := firstIndex + 1
+	secondIndex := remainingStart + firstHighestIndex(bank[remainingStart:])
+
+	return 10 * bank[firstIndex] + bank[secondIndex]
+}
+
--- 03/main_test.go
+++ 03/main_test.go
@@ -36,3 +36,42 @@ func TestFirstHighestIndex(t *testing.T) {
 	}
 }
 
+func TestLargestJolt(t *testing.T) {
+	cases := []struct {
+		name string
+		bank []int
+		expected int
+	}{
+		{
+			"Given1",
+			[]int{ 9, 8, 7, 6, 5, 4, 3, 2, 1, 1, 1, 1, 1, 1, 1 },
+			98,
+		},
+		{
+			"Given2",
+			[]int{ 8, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 9 },
+			89,
+		},
+		{
+			"Given3",
+			[]int{ 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 3, 4, 2, 7, 8 },
+			78,
+		},
+		{
+			"Given4",
+			[]int{ 8, 1, 8, 1, 8, 1, 9, 1, 1, 1, 1, 2, 1, 1, 1 },
+			92,
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			actual := largestJolt(c.bank)
+			
+			if actual != c.expected {
+				t.Errorf("Expected %d to be %d", actual, c.expected)
+			}
+		})
+	}
+}
+
