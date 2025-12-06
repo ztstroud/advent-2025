b9cb1b3578e66a52a7bd61ee7ee19d7b66af261a
Zak Stroud <ztstroud@gmail.com>
2025-12-05 22:19:27 -0800

    feat(05): solve puzzle from file

--- 05/main.go
+++ 05/main.go
@@ -1,6 +1,14 @@
 package main
 
-import "slices"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
 
 type Span struct{ start, end uint64 }
 
@@ -63,5 +71,69 @@ func inAnySpan(val uint64, spans []Span) bool {
 	}
 
 	return false
+}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("You must specify an input file")
+	}
+
+	path := os.Args[1]
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatalf("Failed to read from file: %s\n%v\n", path, err)
+	}
+
+	defer file.Close()
+
+	spans := make([]Span, 0)
+	vals := make([]uint64, 0)
+
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		text := scanner.Text()
+
+		if text == "" {
+			break
+		}
+
+		spanSrc := strings.Split(text, "-")
+		if len(spanSrc) != 2 {
+			log.Fatalf("Invalid range: %s\n", text)
+		}
+
+		lower, err := strconv.ParseInt(spanSrc[0], 10, 64)
+		if err != nil {
+			log.Fatalf("Failed to parse lower bound: %v\n", err)
+		}
+
+		upper, err := strconv.ParseInt(spanSrc[1], 10, 64)
+		if err != nil {
+			log.Fatalf("Failed to parse upper bound: %v\n", err)
+		}
+
+		spans = append(spans, Span{ uint64(lower), uint64(upper) })
+	}
+
+	for scanner.Scan() {
+		val, err := strconv.ParseInt(scanner.Text(), 10, 64)
+		if err != nil {
+			log.Fatalf("Failed to parse value: %v\n", err)
+		}
+
+		vals = append(vals, uint64(val))
+	}
+
+	merged := mergeSpans(spans)
+
+	count := 0
+	for _, val := range vals {
+		if inAnySpan(val, merged) {
+			count += 1
+		}
+	}
+
+	fmt.Printf("In range count: %d\n", count)
 }
 
